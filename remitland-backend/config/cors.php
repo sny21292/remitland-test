7e57a6ebc18755c9b06db8a39c356035a22a9591
Sunil Sharma <sunil@adly.com>
2026-03-15 21:48:44 +0530

    Added a deployment script for github actions and then added SSL certificate

--- remitland-backend/config/cors.php
+++ remitland-backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://remitland.duckdns.org'],
+    'allowed_origins' => ['http://localhost:3000', 'http://remitland.duckdns.org', 'https://remitland.duckdns.org'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
